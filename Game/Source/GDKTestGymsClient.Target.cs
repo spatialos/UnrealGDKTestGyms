76b6352a84a3d16d06bc7486325ec31ba6047b5c
Vlad Barvinko <vladbarvinko@improbable.io>
2019-12-03 12:09:04 +0000

    add client target

---
+++ Game/Source/GDKTestGymsClient.Target.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GDKTestGymsClientTarget : TargetRules
+{
+	public GDKTestGymsClientTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Client;
+		ExtraModuleNames.Add("GDKTestGyms");
+	}
+}
