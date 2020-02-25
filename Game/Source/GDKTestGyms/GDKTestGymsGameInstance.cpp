b5e3b82108416b71c03cddb2fd4839e656333661
Michael Samiec <michaelsamiec@improbable.io>
2020-02-25 12:15:55 +0000

    Add fps output, used by NFR pipeline (#40)
    * Add fps output, used by NFR pipeline
    
    * PR feedback

---
+++ Game/Source/GDKTestGyms/GDKTestGymsGameInstance.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "GDKTestGymsGameInstance.h"
+
+#include "EngineMinimal.h"
+
+void UGDKTestGymsGameInstance::Init()
+{
+	Super::Init();
+
+	if (GEngine)
+	{
+		GEngine->SetMaxFPS(90.f);
+	}
+
+	TickDelegate = FTickerDelegate::CreateUObject(this, &UGDKTestGymsGameInstance::Tick);
+	TickDelegateHandle = FTicker::GetCoreTicker().AddTicker(TickDelegate);
+}
+
+bool UGDKTestGymsGameInstance::Tick(float DeltaSeconds)
+{
+	float Alpha = 0.8f;
+	float CurrFPS = 1.0f / DeltaSeconds; // TODO - we should remove FApp::GetIdleTime() from DeltaSeconds once the NFR regex is fixed
+	AverageFPS = (Alpha * AverageFPS) + ((1.0f - Alpha) * CurrFPS);
+
+	SecondsSinceFPSLog += DeltaSeconds;
+
+	if (SecondsSinceFPSLog > 1.0f) 
+	{
+		SecondsSinceFPSLog = 0.0f;
+#if !WITH_EDITOR // Don't pollute logs in editor
+		UE_LOG(LogTemp, Display, TEXT("FramesPerSecond is %f"), AverageFPS);
+#endif
+	}
+
+	return true;
+}
