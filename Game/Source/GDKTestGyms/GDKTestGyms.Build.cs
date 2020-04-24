92234374bb3cdb328a0f50bc648803272ae12df9
Danny Birch <danielbirch@improbable.io>
2020-04-24 22:31:36 +0100

    Feature/deterministic nfr runs (#67)
    * Removed unused code
    
    * Initial pass on seeded random run values
    
    * PR comments
    
    * Spacing fixes
    
    * VS..
    
    * Manual merge SimulatedPlayerCharacter_BP
    
    * Roam comment
    
    * Client RPC method name
    
    * PR comments + simplified logic around called client RPC on DeterministicBlackboardValues
    
    * Fix default blackboard values
    
    * Spawn NPCs at their first point to run to
    
    * Fix NPCs
    
    * Revert "Fix NPCs"
    
    Revert SimulatedPlayerCharacter_BP
    
    * Fix NPCs
    
    * Spawned too far away from cluster
    
    * Log + attempt to ensure possession always occurs on the spawned AI controller
    
    * Unreal!
    
    * Test spam
    
    * missed change
    
    * Log on BeginPlay + gain authority
    
    * Retry local control test
    
    * Spawn controller fixes
    
    * Update Game/Source/GDKTestGyms/Private/DeterministicBlackboardValues.cpp
    
    Co-authored-by: Michael Samiec <michaelsamiec@improbable.io>

--- Game/Source/GDKTestGyms/GDKTestGyms.Build.cs
+++ Game/Source/GDKTestGyms/GDKTestGyms.Build.cs
@@ -22,7 +22,9 @@ public class GDKTestGyms : ModuleRules
 				"OnlineSubsystemUtils",
 				"PhysXVehicles",
 				"SpatialGDK",
-				"ReplicationGraph"
+				"ReplicationGraph",
+				"AIModule",
+				"NavigationSystem"
 			});
 	}
 }
