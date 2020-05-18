91a4beffaf6f18bd8df44e24dcb25a5f9f66e91b
Danny Birch <danielbirch@improbable.io>
2020-05-18 16:42:04 +0100

    Comment

--- Game/Source/GDKTestGyms/Public/UserExperienceComponent.h
+++ Game/Source/GDKTestGyms/Public/UserExperienceComponent.h
@@ -8,6 +8,23 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogUserExperienceComponent, Log, All);
 
+// User experience metric
+//
+// This component is designed to track a user experience in a deployment. 
+// It tracks 2 key metrics, these are round trip latency and world
+// updates. The player controlled component will fire a server RPC 
+// every second and track the time it was sent, the server handles
+// this RPC and sends back a key which identifies the start time. 
+//
+// A second metric is world view lateness, this looks at all the 
+// other UserExperienceComponent objects in view and tracks how
+// often these are updated. This gives us a definition of the 
+// world update rate, this is used to average the time 
+// between updates. 
+//
+// Both of these values are averaged by the server and reported. 
+// See BenchmarkGymGameMode::ServerUpdateNFRTestMetrics
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class GDKTESTGYMS_API UUserExperienceComponent : public UActorComponent
 {
