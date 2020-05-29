65cca807449ad5005f468583fbd3cfb339e673f4
Danny Birch <danielbirch@improbable.io>
2020-05-29 18:35:32 +0100

    Updating FPS metric to match GDK (2 minute window average)

--- Game/Source/GDKTestGyms/GDKTestGymsGameInstance.cpp
+++ Game/Source/GDKTestGyms/GDKTestGymsGameInstance.cpp
@@ -12,7 +12,7 @@ void UGDKTestGymsGameInstance::Init()
 
 	TickDelegate = FTickerDelegate::CreateUObject(this, &UGDKTestGymsGameInstance::Tick);
 	TickDelegateHandle = FTicker::GetCoreTicker().AddTicker(TickDelegate);
-
+	TickWindowTotal = 0;
 	GetEngine()->NetworkFailureEvent.AddUObject(this, &UGDKTestGymsGameInstance::NetworkFailureEventCallback);
 }
 
@@ -23,6 +23,41 @@ void UGDKTestGymsGameInstance::OnStart()
 	{
 		GetEngine()->SetMaxFPS(60.0f);
 	}
+}
+
+float UGDKTestGymsGameInstance::AddAndCalcFps(int64 NowReal, float DeltaS)
+{
+	int64 DeltaTicks = FTimespan::FromSeconds(DeltaS).GetTicks();
+	TicksForFPS.Push(TPair<int64, int64>(NowReal, DeltaTicks)); 
+	TickWindowTotal += DeltaTicks;
+
+	int64 MinRange = NowReal - FTimespan::FromMinutes(2.0).GetTicks();
+	
+	// Trim samples
+	int NumToRemove = 0;
+	for(int i = 0; i < TicksForFPS.Num(); i++)
+	{
+		const FPSTimePoint& TimePoint = TicksForFPS[i];
+		if (TimePoint.Key > MinRange) // Find the first valid sample
+		{
+			NumToRemove = i;
+			break;
+		}
+	}
+	if (NumToRemove > 0)
+	{
+		for (int i = 0; i < NumToRemove; i++)
+		{
+			TickWindowTotal -= TicksForFPS[i].Value;
+		}
+		memmove(&TicksForFPS[0], &TicksForFPS[NumToRemove], (TicksForFPS.Num() - NumToRemove) * sizeof(FPSTimePoint));
+		TicksForFPS.SetNum(TicksForFPS.Num() - NumToRemove);
+	}
+	if (TicksForFPS.Num() > 0)
+	{
+		return static_cast<float>(TicksForFPS.Num()) / FTimespan(TickWindowTotal).GetTotalSeconds(); // 1.0f / Total / Samples 
+	}
+	return 60.0f; // If there are no samples return the ideal
 }
 
 void UGDKTestGymsGameInstance::NetworkFailureEventCallback(UWorld* World, UNetDriver* NetDriver, ENetworkFailure::Type FailureType, const FString& ErrorString)
@@ -45,18 +80,15 @@ void UGDKTestGymsGameInstance::NetworkFailureEventCallback(UWorld* World, UNetDr
 }
 
 bool UGDKTestGymsGameInstance::Tick(float DeltaSeconds)
-{
-	float Alpha = 0.8f;
-	float CurrFPS = 1.0f / DeltaSeconds; // TODO - we should remove FApp::GetIdleTime() from DeltaSeconds once the NFR regex is fixed
-	AverageFPS = (Alpha * AverageFPS) + ((1.0f - Alpha) * CurrFPS);
-
+{	
+	float FPS = AddAndCalcFps(FDateTime::Now().GetTicks(), DeltaSeconds);
 	SecondsSinceFPSLog += DeltaSeconds;
 
 	if (SecondsSinceFPSLog > 1.0f) 
 	{
 		SecondsSinceFPSLog = 0.0f;
 #if !WITH_EDITOR // Don't pollute logs in editor
-		UE_LOG(LogTemp, Display, TEXT("FramesPerSecond is %f"), AverageFPS);
+		UE_LOG(LogTemp, Display, TEXT("FramesPerSecond is %f"), FPS);
 #endif
 	}
 
--- Game/Source/GDKTestGyms/GDKTestGymsGameInstance.h
+++ Game/Source/GDKTestGyms/GDKTestGymsGameInstance.h
@@ -20,9 +20,14 @@ public:
 	void NetworkFailureEventCallback(UWorld* World, UNetDriver* NetDriver, ENetworkFailure::Type FailureType, const FString& ErrorString);
 
 private:
+	using FPSTimePoint = TPair<int64, int64>; // Real, FrameDelta
+	int64 TickWindowTotal;
+	TArray<FPSTimePoint> TicksForFPS;
+	float AddAndCalcFps(int64 NowReal, float DeltaS);
+
 	FTickerDelegate TickDelegate;
 	FDelegateHandle TickDelegateHandle;
 
 	float AverageFPS = 60.0f;
-	float SecondsSinceFPSLog = 0.0f;
+	float SecondsSinceFPSLog = 1.0f;
 };
