19984bc51238a9bc1d05cddec2e85f03e2293b57
Danny Birch <danielbirch@improbable.io>
2020-06-09 18:20:25 +0100

    Remove old way of testing FPS

--- Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
+++ Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
@@ -48,8 +48,6 @@ ABenchmarkGymGameModeBase::ABenchmarkGymGameModeBase()
 	, bHasFpsFailed(false)
 	, bHasClientFpsFailed(false)
 	// These values need to match the GDK scenario validation equivalents
-	, MinAcceptableFPS(20.0f) // Same for both client and server currently
-	, MinDelayFPS(120.0f)
 	, ActivePlayers(0)
 {
 	SetReplicates(true);
@@ -152,11 +150,6 @@ void ABenchmarkGymGameModeBase::TickPlayersConnectedCheck(float DeltaSeconds)
 
 void ABenchmarkGymGameModeBase::TickFPSCheck(float DeltaSeconds)
 {
-	if (MinDelayFPS > 0.0f)
-	{
-		MinDelayFPS -= DeltaSeconds;
-	}
-
 	const UNFRConstants* Constants = UNFRConstants::Get(GetWorld());
 	check(Constants);
 	if (Constants->SamplesForFPSValid() && !bHasFpsFailed && GetWorld() != nullptr)
