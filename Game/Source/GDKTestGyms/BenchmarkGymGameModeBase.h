9f9d1d165d4e3ce84c31fc560e62d7f1db372ee7
Michael Samiec <michaelsamiec@improbable.io>
2020-06-20 12:06:22 +0100

    period

--- Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.h
+++ Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.h
@@ -24,7 +24,7 @@ protected:
 	// Total number of players that will attempt connect.
 	int32 ExpectedPlayers;
 
-	// Total number of players that must connect for the test to pass. Must be less than ExpectedPlayers
+	// Total number of players that must connect for the test to pass. Must be less than ExpectedPlayers.
 	// This allows some accepted client flakes without failing the overall test.
 	int32 RequiredPlayers;
 
