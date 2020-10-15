41b119ab38d65c8611de6d818517809803cc8687
Matthew Sandford <matthewsandford@improbable.io>
2020-10-15 16:49:03 +0100

    Adding c++ test component and neatening some stuff up.

---
+++ Game/Source/GDKTestGyms/Private/EventTracerComponent.cpp
@@ -0,0 +1,69 @@
+#include "EventTracerComponent.h"
+
+#include "Interop/Connection/SpatialEventTracerUserInterface.h"
+#include "Interop/Connection/SpatialTraceEventBuilder.h"
+#include "Net/UnrealNetwork.h"
+
+UEventTracerComponent::UEventTracerComponent()
+{
+	SetIsReplicatedByDefault(true);
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UEventTracerComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UEventTracerComponent, bTestBool);
+}
+
+void UEventTracerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	if (OwnerHasAuthority() && bUseEventTracing)
+	{
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UEventTracerComponent::TimerFunction, 5.0f, true);
+	}
+}
+
+void UEventTracerComponent::TimerFunction()
+{
+	// Create a user trace event for sending a property update
+	FString SpanId = USpatialEventTracerUserInterface::CreateSpanId(this);
+	USpatialEventTracerUserInterface::TraceEvent(this, SpatialGDK::FSpatialTraceEventBuilder("user.send_component_property").GetEvent(), SpanId);
+	USpatialEventTracerUserInterface::AddLatentSpanId(this, this, SpanId);
+
+	bTestBool = !bTestBool;
+
+	// Create a user trace event for sending an RPC
+	SpanId = USpatialEventTracerUserInterface::CreateSpanId(this);
+	USpatialEventTracerUserInterface::TraceEvent(this, SpatialGDK::FSpatialTraceEventBuilder("user.send_rpc").GetEvent(), SpanId);
+
+	USpatialEventTracerUserInterface::AddSpanIdToStack(this, SpanId);
+	RunOnClient();
+	USpatialEventTracerUserInterface::PopSpanIdFromStack(this);
+}
+
+void UEventTracerComponent::OnRepBool()
+{
+	if (!OwnerHasAuthority())
+	{
+		// Create a user trace event for receiving a property update
+		FString SpanId = USpatialEventTracerUserInterface::CreateSpanId(this);
+		USpatialEventTracerUserInterface::TraceEvent(this, SpatialGDK::FSpatialTraceEventBuilder("user.receive_component_property").GetEvent(), SpanId);
+	}
+}
+
+void UEventTracerComponent::RunOnClient_Implementation()
+{
+	// Create a user trace event for receiving an RPC
+	FString SpanId = USpatialEventTracerUserInterface::CreateSpanId(this);
+	USpatialEventTracerUserInterface::TraceEvent(this, SpatialGDK::FSpatialTraceEventBuilder("user.process_rpc").GetEvent(), SpanId);
+}
+
+bool UEventTracerComponent::OwnerHasAuthority() const
+{
+	AActor* Owner = GetOwner();
+	return Owner != nullptr && Owner->HasAuthority();
+}
---
+++ Game/Source/GDKTestGyms/Public/EventTracerComponent.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EventTracerComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GDKTESTGYMS_API UEventTracerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UEventTracerComponent();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool bUseEventTracing = true;
+
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+
+	UPROPERTY(ReplicatedUsing = OnRepBool)
+	bool bTestBool;
+
+	FTimerHandle TimerHandle;
+
+	UFUNCTION()
+	void TimerFunction();
+
+	UFUNCTION(Reliable, Client)
+	void RunOnClient();
+
+	UFUNCTION()
+	void OnRepBool();
+
+	bool OwnerHasAuthority() const;
+	
+};
