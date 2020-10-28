1bfba1a027691053dbb2c6d44111bfbf7c0bd37e
Ken.Yu <kenyu@improbable.io>
2020-10-28 17:46:45 +0800

    Change RTT and UpdateTimeDelta unit to milisecond (#220)

--- Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.h
+++ Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.h
@@ -74,10 +74,10 @@ protected:
 private:
 	// Test scenarios
 
-	double AveragedClientRTTSeconds; // The stored average of all the client RTTs
-	double AveragedClientUpdateTimeDeltaSeconds; // The stored average of the client view delta.
-	int32 MaxClientRoundTripSeconds; // Maximum allowed roundtrip
-	int32 MaxClientUpdateTimeDeltaSeconds;
+	double AveragedClientRTTMS; // The stored average of all the client RTTs
+	double AveragedClientUpdateTimeDeltaMS; // The stored average of the client view delta.
+	int32 MaxClientRoundTripMS; // Maximum allowed roundtrip
+	int32 MaxClientUpdateTimeDeltaMS;
 	bool bHasUxFailed;
 	bool bHasFpsFailed;
 	bool bHasDonePlayerCheck;
@@ -125,8 +125,8 @@ private:
 
 	void SetTotalNPCs(int32 Value);
 
-	double GetClientRTT() const { return AveragedClientRTTSeconds; }
-	double GetClientUpdateTimeDelta() const { return AveragedClientUpdateTimeDeltaSeconds; }
+	double GetClientRTT() const { return AveragedClientRTTMS; }
+	double GetClientUpdateTimeDelta() const { return AveragedClientUpdateTimeDeltaMS; }
 	double GetPlayersConnected() const { return static_cast<double>(ActivePlayers); }
 	double GetTotalMigrationValid() const { return !bHasActorMigrationCheckFailed ? 1.0 : 0.0; }
 	double GetFPSValid() const { return !bHasFpsFailed ? 1.0 : 0.0; }
