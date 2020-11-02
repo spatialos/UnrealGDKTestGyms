36f58f5f4c2ef70e1b2938e2669fc8a6caac6130
MatthewSandfordImprobable <matthewsandford@improbable.io>
2020-11-02 12:09:08 +0000

    User Event Tracing (#216)
    * Basic testing environment for event tracing.
    
    * Clean up blueprint
    
    * Adding instructions to turn on event tracing.
    
    * Adding missing component update test
    
    * Updates
    
    * Update map
    
    * User tests for RPCs
    
    * Adding c++ test component and neatening some stuff up.
    
    * Updates
    
    * Remove redundant blueprint
    
    * Updates
    
    * Feedback
    
    * Using new API
    
    * Update use of API
    
    * Updates for API
    
    * Updates
    
    * Updates
    
    * Feedback
    
    * Ensure up to date
    
    * Vali feedback
    
    * Update
    
    * Feedback

---
+++ Game/Source/GDKTestGyms/Public/EventTracerComponent.h
@@ -0,0 +1,46 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EventTracerComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GDKTESTGYMS_API UEventTracerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UEventTracerComponent();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool bUseEventTracing = true;
+
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+
+	UPROPERTY(ReplicatedUsing = OnRepTestInt)
+	int32 TestInt;
+
+	FTimerHandle TimerHandle;
+
+	FString UserSendRPCEventName;
+	FString UserProcessRPCEventName;
+	FString UserSendComponentPropertyEventName;
+	FString UserReceiveComponentPropertyEventName;
+
+	UFUNCTION()
+	void TimerFunction();
+
+	UFUNCTION(Reliable, Client)
+	void RunOnClient();
+
+	UFUNCTION()
+	void OnRepTestInt();
+
+	bool OwnerHasAuthority() const;
+	
+};
