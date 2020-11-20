ce1712b8315e7da48cbe2aac198c59084b057f6b
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2020-11-20 12:27:14 +0000

    [UNR-4414] Gameplay cues gym (#227)
    Adds a character class with a properly added ASC, such that it can be accessed via IAbilitySystemInterface.
    Adds the gameplay cues gym with ways to test gameplay cues being added/removed as well as executed.

---
+++ Game/Source/GDKTestGyms/Public/GasCharacter.h
@@ -0,0 +1,33 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GDKTestGyms/GDKTestGymsCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "GasCharacter.generated.h"
+
+/**
+ * A GDK Test Gyms Character with an Ability System Component added and set up to correctly initialise.
+ */
+UCLASS()
+class GDKTESTGYMS_API AGasCharacter : public AGDKTestGymsCharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+	// Implement IAbilitySystemInterface.
+	UAbilitySystemComponent* GetAbilitySystemComponent() const override	
+	{
+		return AbilitySystemComponent;
+	}
+
+public:
+	AGasCharacter();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Abilities)
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_Controller() override;
+};
