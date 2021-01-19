5ed2875b22fe24b1793e33774ec3ceaaabff2504
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2021-01-19 13:50:42 +0000

    [UNR-4827] Move GAS tests into GDKTestGyms (#279)
    Move GAS tests into their own editor-only module, re-add to map, add build dependencies, add LogGymsSpatialFunctionalTest

--- Game/Source/GDKTestGymsFunctionalTests/Tests/PredictedGameplayCuesTest/GA_ApplyCueEffect.cpp
+++ Game/Source/GDKTestGymsFunctionalTests/Tests/PredictedGameplayCuesTest/GA_ApplyCueEffect.cpp
@@ -29,7 +29,7 @@ void UGA_ApplyCueEffect::ActivateAbility(const FGameplayAbilitySpecHandle Handle
 	{
 		if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
 		{
-			EndAbility(Handle, ActorInfo, ActivationInfo, false, true);
+			EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, true);
 			return;
 		}
 
@@ -44,6 +44,6 @@ void UGA_ApplyCueEffect::ActivateAbility(const FGameplayAbilitySpecHandle Handle
 									   UGE_ExecuteCue::StaticClass()->GetDefaultObject<UGameplayEffect>(), 0.0f);
 		}
 
-		EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);
 	}
 }
