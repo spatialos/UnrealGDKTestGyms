f18fc41b5207965140f04ca3b1fdc321b5e1e86a
Simon Sarginson <simonsarginson@improbable.io>
2021-02-02 11:35:31 +0000

    improved comments, fixed up variable name

--- ci/generate-pipeline-steps.sh
+++ ci/generate-pipeline-steps.sh
@@ -5,16 +5,14 @@ if [[ -n "${DEBUG-}" ]]; then
     set -x
 fi
 
-# Download the unreal-engine.version file from the GDK repo so we can run the example project builds on the same versions the GDK was run against.
-# This is not the pinnacle of engineering, as we rely on GitHub's web interface to download the file, but it seems like GitHub disallows git archive
-# which would be our other option for downloading a single file.
-# Also resolve the GDK branch to run against. The order of priority is:
+# Detection code copied from ci folder from UnrealGDKExampleProject 
+# Resolve the GDK branch to run against. The order of priority is:
 # GDK_BRANCH envvar > same-name branch as the branch we are currently on > UnrealGDKVersion.txt > "master".
 GDK_BRANCH_LOCAL="${GDK_BRANCH:-}"
 if [ -z "${GDK_BRANCH_LOCAL}" ]; then
     GDK_REPO_HEADS=$(git ls-remote --heads "git@github.com:spatialos/UnrealGDK.git" "${BUILDKITE_BRANCH}")
-    EXAMPLEPROJECT_REPO_HEAD="refs/heads/${BUILDKITE_BRANCH}"
-    if echo "${GDK_REPO_HEADS}" | grep -qF "${EXAMPLEPROJECT_REPO_HEAD}"; then
+    TESTGYMS_REPO_HEAD="refs/heads/${BUILDKITE_BRANCH}"
+    if echo "${GDK_REPO_HEADS}" | grep -qF "${TESTGYMS_REPO_HEAD}"; then
         GDK_BRANCH_LOCAL="${BUILDKITE_BRANCH}"
     else
         GDK_VERSION=$(cat UnrealGDKVersion.txt)
