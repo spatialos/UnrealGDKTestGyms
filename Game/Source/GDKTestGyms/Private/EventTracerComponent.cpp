e19d309dec973d0e79a243d911a4b3ff6e1c91ec
Matthew Sandford <matthewsandford@improbable.io>
2021-05-12 16:25:25 +0100

    Increasing the rate at which events are fired.

--- Game/Source/GDKTestGyms/Private/EventTracerComponent.cpp
+++ Game/Source/GDKTestGyms/Private/EventTracerComponent.cpp
@@ -25,7 +25,7 @@ void UEventTracerComponent::BeginPlay()
 
 	if (OwnerHasAuthority() && bUseEventTracing)
 	{
-		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UEventTracerComponent::TimerFunction, 5.0f, true);
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UEventTracerComponent::TimerFunction, 1.0f, true);
 	}
 }
 
