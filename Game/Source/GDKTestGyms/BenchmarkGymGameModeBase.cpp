46c423547770bde08b0cb7f44dd2cc5a8789d6dc
Matthew Sandford <matthewsandford@improbable.io>
2021-05-14 14:57:16 +0100

    Fix

--- Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
+++ Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
@@ -895,7 +895,7 @@ void ABenchmarkGymGameModeBase::CheckTotalCountsForActors()
 					{
 						const TSubclassOf<AActor> ActorClass2 = ActorCountPair2.Key;
 						const FActorCountInfo ActorCountInfo = ActorCountPair2.Value;
-						NFR_LOG(LogBenchmarkGymGameModeBase, Error, TEXT("%s - %d"), ActorClass2->GetName(), ActorCountInfo.GetTotal());
+						NFR_LOG(LogBenchmarkGymGameModeBase, Error, TEXT("%s - %d"), *ActorClass2->GetName(), ActorCountInfo.GetTotal());
 					}
 
 				}
@@ -903,7 +903,7 @@ void ABenchmarkGymGameModeBase::CheckTotalCountsForActors()
 				NFR_LOG(LogBenchmarkGymGameModeBase, Error, TEXT("Temp"));
 				for (const auto& ActorCountPair2 : TempTotalActorCounts)
 				{
-					NFR_LOG(LogBenchmarkGymGameModeBase, Error, TEXT("%s - %d"), ActorCountPair2.Key->GetName(), ActorCountPair2.Value);
+					NFR_LOG(LogBenchmarkGymGameModeBase, Error, TEXT("%s - %d"), *ActorCountPair2.Key->GetName(), ActorCountPair2.Value);
 				}
 
 				bHasActorCountFailed = true;
