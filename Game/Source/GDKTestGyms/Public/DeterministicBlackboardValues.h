62b802dbd13ffd1aae1821f9026aee8bdc1412d0
Teri Drummond <teridrummond@improbable.io>
2021-05-28 17:32:59 +0100

    Bugfix/unr-5543 (#400)
    * Fixed things up so points to run to are handed over the zoning border and correctly set up on newly spawned controllers, and that old controllers are destroyed when they lose authority
    
    * revert change to BuiltData
    
    * Added a state bool that is handed over and tells the NPC which target it is trying to reach. Updated behavior tree to choose target based on this bool.
    
    * Address Danny's comment to change bool name, delete temp file

--- Game/Source/GDKTestGyms/Public/DeterministicBlackboardValues.h
+++ Game/Source/GDKTestGyms/Public/DeterministicBlackboardValues.h
@@ -14,11 +14,21 @@ class GDKTESTGYMS_API UDeterministicBlackboardValues : public UActorComponent
 {
 	GENERATED_BODY()
 public:
-	void ApplyValues();
+
+	void InitialApplyBlackboardValues();
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyBlackboardValues();
+
+	UFUNCTION(BlueprintCallable)
+	void SwapTarget();
 
 	UFUNCTION(Client, Reliable)
 	void ClientSetBlackboardAILocations(const FBlackboardValues& InBlackboardValues);
+
 protected:
 	FTimerHandle TimerHandle;
+
+	UPROPERTY(Handover)
 	FBlackboardValues BlackboardValues;
 };
