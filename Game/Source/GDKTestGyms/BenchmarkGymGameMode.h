74c916d89fa7c43c9a2339f13bea1814c3fd5b42
MatthewSandfordImprobable <matthewsandford@improbable.io>
2021-06-28 12:50:30 +0100

    Generic grid spawning in test gyms project (#420)

--- Game/Source/GDKTestGyms/BenchmarkGymGameMode.h
+++ Game/Source/GDKTestGyms/BenchmarkGymGameMode.h
@@ -10,11 +10,91 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogBenchmarkGymGameMode, Log, All);
 
-typedef TPair<TWeakObjectPtr<AController>, int> ControllerIntegerPair;
+using ControllerIntegerPair = TPair<TWeakObjectPtr<AController>, int>;
 
-/**
- *
- */
+class APlayerStart;
+
+USTRUCT()
+struct FSpawnCluster
+{
+	GENERATED_USTRUCT_BODY()
+
+	UWorld* World = nullptr;
+	FVector WorldPosition;
+	float Width;
+	float Height;
+	int32 MaxSpawnPoints;
+	float MinDistanceBetweenSpawnPoints;
+
+	bool GenerateSpawnPoints();
+	const TArray<APlayerStart*>& GetSpawnPointActors() { return SpawnPointActors; };
+
+private:
+
+	TArray<APlayerStart*> SpawnPointActors;
+};
+
+USTRUCT()
+struct FSpawnArea
+{
+	GENERATED_USTRUCT_BODY()
+
+	enum AreaType : uint8
+	{
+		Zone,
+		Boundary,
+		Count
+	};
+
+	UWorld* World = nullptr;
+	FVector WorldPosition;
+	AreaType Type;
+	float Width;
+	float Height;
+	int32 NumClusters;
+	int32 MaxSpawnPointsPerCluster;
+	float MinDistanceBetweenClusters;
+	float MinDistanceBetweenSpawnPoints;
+
+	bool GenerateSpawnClusters();
+	const TArray<APlayerStart*>& GetSpawnPointActors() { return SpawnPointActors; };
+	AActor* GetSpawnPointActorByIndex(const int32 Index) const;
+
+private:
+
+	TArray<FSpawnCluster> SpawnClusters;
+	TArray<APlayerStart*> SpawnPointActors;
+};
+
+UCLASS()
+class GDKTESTGYMS_API USpawnManager: public UObject
+{
+	GENERATED_BODY()
+public:
+
+	// Will create SpawnAreas for zones and boundaries for given parameters and add them to the member SpawnAreas.
+	// Each SpawnArea will be set up to only create a certain number of spawn points.
+	// This function should allow you to vary how many NPCs/Simplayers are spawned in the centre of zones or on boundaries.
+	void GenerateSpawnAreas(const int32 ZoneRows, const int32 ZoningCols, const int32 ZoneWidth, const int32 ZoneHeight,
+		const int32 ZoneClusters, const int32 BoundaryClusters,
+		const int32 MaxSpawnPointsPerCluster, const float MinDistanceBetweenClusters, const float MinDistanceBetweenSpawnPoints);
+
+	AActor* GetSpawnPointActorByIndex(const int32 Index) const;
+	int32 GetNumSpawnPoints() const;
+
+	void ClearSpawnPoints();
+
+	void ForEachArea(TFunctionRef<void(FSpawnArea& ZoneArea)> Predicate, const FSpawnArea::AreaType AreaType = FSpawnArea::AreaType::Count);
+
+private:
+
+	UPROPERTY()
+	TArray<FSpawnArea> SpawnAreas;
+
+	UPROPERTY()
+	TArray<APlayerStart*> SpawnPointActors;
+};
+
 UCLASS()
 class GDKTESTGYMS_API ABenchmarkGymGameMode : public ABenchmarkGymGameModeBase
 {
@@ -23,45 +103,88 @@ public:
 	ABenchmarkGymGameMode();
 	AActor* FindPlayerStart_Implementation(AController* Player, const FString& IncomingName) override;
 
+	virtual void BeginPlay() override;
+
 protected:
 
+	UPROPERTY()
+	USpawnManager* SpawnManager;
+
+	UPROPERTY(EditAnywhere, NoClear, BlueprintReadOnly, Category = Classes)
+	TSubclassOf<AActor> DropCubeClass;
+
+	float DistBetweenClusters;
+	float DistBetweenSpawnPoints;
+	float PercentageSpawnPointsOnWorkerBoundaries;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void StartCustomNPCSpawning();
 	virtual void BuildExpectedActorCounts() override;
-	virtual void OnAnyWorkerFlagUpdated(const FString& FlagName, const FString& FlagValue) override;
+
+	virtual void ReadCommandLineArgs(const FString& CommandLine) override;
+	virtual void ReadWorkerFlagValues(USpatialWorkerFlags* SpatialWorkerFlags) override;
+	virtual void BindWorkerFlagDelegates(USpatialWorkerFlags* SpatialWorkerFlags) override;
+
+	virtual void AddSpatialMetrics(USpatialMetrics* SpatialMetrics) override;
+
+	virtual void OnTotalNPCsUpdated_Implementation(int32 Value) override;
 
 private:
+
 	TArray<FBlackboardValues> PlayerRunPoints;
 	TArray<FBlackboardValues> NPCRunPoints;
-	void GenerateTestScenarioLocations();
 
 	TArray<ControllerIntegerPair> AIControlledPlayers;
 
-	virtual void Tick(float DeltaSeconds) override;
-	virtual void ParsePassedValues() override;
+	bool bHasCreatedSpawnPoints;
 
-	// Custom density spawning parameters.
-	bool bInitializedCustomSpawnParameters;
 	// Number of players per cluster. Players only see other players in the same cluster.
 	// Number of generated clusters is Ceil(TotalPlayers / PlayerDensity)
 	int32 PlayerDensity;
-	int32 NumPlayerClusters;
 	int32 PlayersSpawned;
-	UPROPERTY()
-	TArray<AActor*> SpawnPoints;
+	int32 NPCSToSpawn;
+
+	// Actor migration members
+	bool bIsUsingZoning;
+	bool bHasActorMigrationCheckFailed;
+	int32 PreviousTickMigration;
+	typedef TTuple<int32, float> MigrationDeltaPair;
+	TQueue<MigrationDeltaPair> MigrationDeltaHistory;
+	int32 MigrationOfCurrentWorker;
+	float MigrationSeconds;
+	float MigrationCountSeconds;
+	float MigrationWindowSeconds;
+	TMap<FString, float> MapWorkerActorMigration;
+	float MinActorMigrationPerSecond;
+	FMetricTimer ActorMigrationReportTimer;
+	FMetricTimer ActorMigrationCheckTimer;
+	FMetricTimer ActorMigrationCheckDelay;
+
 	UPROPERTY()
 	TMap<int32, AActor*> PlayerIdToSpawnPointMap;
-	TSubclassOf<AActor> DropCubeClass;
-	int32 NPCSToSpawn;
 
 	UPROPERTY()
 	ABenchmarkGymNPCSpawner* NPCSpawner;
 
-	void CheckCmdLineParameters();
+	void GenerateTestScenarioLocations();
 	void ClearExistingSpawnPoints();
-	void StartCustomNPCSpawning();
+	void TryStartCustomNPCSpawning();
+	void GenerateSpawnPoints();
+
+	void TickActorMigration(float DeltaSeconds);
+	void TickNPCSpawning();
+	void TickSimPlayerBlackboardValues();
+
 	void SpawnNPCs(int NumNPCs);
-	void SpawnNPC(const FVector& SpawnLocation, const FBlackboardValues& BlackboardValues);
-	// Generates a grid of points centered at (0, 0), as square-like as possible. A row has a fixed y-value, and a column a fixed x-value.
-	static void GenerateGridSettings(int DistBetweenPoints, int NumPoints, int& NumRows, int& NumCols, int& MinRelativeX, int& MinRelativeY);
-	void GenerateSpawnPointClusters(int NumClusters);
-	void GenerateSpawnPoints(int CenterX, int CenterY, int SpawnPointsNum);
+	bool SpawnNPC(const FVector& SpawnLocation, const FBlackboardValues& BlackboardValues);
+
+	double GetTotalMigrationValid() const { return !bHasActorMigrationCheckFailed ? 1.0 : 0.0; }
+
+	UFUNCTION(CrossServer, Reliable)
+	virtual void ReportMigration(const FString& WorkerID, const float Migration);
+
+	// Worker flag update delegate functions
+	UFUNCTION()
+	void OnPlayerDensityFlagUpdate(const FString& FlagName, const FString& FlagValue);
 };
