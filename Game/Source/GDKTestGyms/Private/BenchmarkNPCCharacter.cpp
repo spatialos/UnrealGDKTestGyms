bc3a01cb28b98e0face4bdb5625a5cc679b53e54
Matthew Sandford <matthewsandford@improbable.io>
2021-07-15 12:22:02 +0100

    Debugging

--- Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
+++ Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
@@ -8,12 +8,53 @@ DEFINE_LOG_CATEGORY(LogBenchmarkNPCCharacter);
 void ABenchmarkNPCCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("BeginPlay"));
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("OnAuthorityGained %s"), *GetName());
 }
 
 void ABenchmarkNPCCharacter::OnAuthorityGained()
 {
-	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("OnAuthorityGained"));
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("OnAuthorityGained %S"), *GetName());
+}
+
+void ABenchmarkNPCCharacter::SpawnDefaultController()
+{
+	if (Controller != nullptr || GetNetMode() == NM_Client)
+	{
+		UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("SpawnDefaultController NM_Client %S"), *GetName());
+		return;
+	}
+
+	if (AIControllerClass != nullptr)
+	{
+		FActorSpawnParameters SpawnInfo;
+		SpawnInfo.Instigator = GetInstigator();
+		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		SpawnInfo.OverrideLevel = GetLevel();
+		SpawnInfo.ObjectFlags |= RF_Transient;	// We never want to save AI controllers into a map
+		AController* NewController = GetWorld()->SpawnActor<AController>(AIControllerClass, GetActorLocation(), GetActorRotation(), SpawnInfo);
+		if (NewController != nullptr)
+		{
+			// if successful will result in setting this->Controller 
+			// as part of possession mechanics
+			UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("SpawnDefaultController Possess %S"), *GetName());
+			NewController->Possess(this);
+		}
+		else
+		{
+			UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("SpawnDefaultController NewController == nullptr %S"), *GetName());
+		}
+	}
+	else
+	{
+		UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("SpawnDefaultController AIControllerClass == nullptr %S"), *GetName());
+	}
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("SpawnDefaultController %S"), *GetName());
+}
+
+void ABenchmarkNPCCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("OnAuthorityGained %S"), *GetName());
 }
 
 void ABenchmarkNPCCharacter::OnAuthorityLost()
