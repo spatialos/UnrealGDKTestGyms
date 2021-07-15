eba12c6a6c8345bd2db99371bb4005408533014e
Matthew Sandford <matthewsandford@improbable.io>
2021-07-15 10:29:13 +0100

    Debugging

--- Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
+++ Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
@@ -2,6 +2,20 @@
 
 #include "BenchmarkNPCCharacter.h"
 
+
+DEFINE_LOG_CATEGORY(LogBenchmarkNPCCharacter);
+
+void ABenchmarkNPCCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("BeginPlay"));
+}
+
+void ABenchmarkNPCCharacter::OnAuthorityGained()
+{
+	UE_LOG(LogBenchmarkNPCCharacter, Log, TEXT("OnAuthorityGained"));
+}
+
 void ABenchmarkNPCCharacter::OnAuthorityLost()
 {
 	Super::OnAuthorityLost();
--- Game/Source/GDKTestGyms/Public/BenchmarkNPCCharacter.h
+++ Game/Source/GDKTestGyms/Public/BenchmarkNPCCharacter.h
@@ -6,12 +6,16 @@
 #include "GameFramework/Character.h"
 #include "BenchmarkNPCCharacter.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogBenchmarkNPCCharacter, Log, All);
+
 UCLASS()
 class GDKTESTGYMS_API ABenchmarkNPCCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
+	virtual void BeginPlay() override;
+	virtual void OnAuthorityGained() override;
 	virtual void OnAuthorityLost() override;
 
 };
