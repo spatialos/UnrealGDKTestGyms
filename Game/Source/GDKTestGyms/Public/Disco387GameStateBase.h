a2879c05c4b51013405c7b6d24b51a6ed4ad595d
Michael Samiec <michaelsamiec@improbable.io>
2021-07-15 17:27:24 +0100

    Add custom config overrides

---
+++ Game/Source/GDKTestGyms/Public/Disco387GameStateBase.h
@@ -0,0 +1,19 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Disco387GameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GDKTESTGYMS_API ADisco387GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+};
