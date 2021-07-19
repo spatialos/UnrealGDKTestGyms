0ebf9654452c014076d7127f42bdc9f64de06c2a
Matthew Sandford <matthewsandford@improbable.io>
2021-07-19 14:20:06 +0100

    Remove test

--- Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
+++ Game/Source/GDKTestGyms/Private/BenchmarkNPCCharacter.cpp
@@ -1,12 +1,12 @@
 // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
 
 #include "BenchmarkNPCCharacter.h"
-/*
+
 void ABenchmarkNPCCharacter::OnAuthorityGained()
 {
 	Super::OnAuthorityGained();
 }
-*/
+
 void ABenchmarkNPCCharacter::OnAuthorityLost()
 {
 	Super::OnAuthorityLost();
