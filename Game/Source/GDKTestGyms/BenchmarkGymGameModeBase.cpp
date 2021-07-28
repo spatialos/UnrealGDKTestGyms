c55051793d7fe6595e96f21453fda353a5b93ebb
Miron Zelina <mironzelina@improbable.io>
2021-07-28 11:59:32 +0100

    Turn density buckets on

--- Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
+++ Game/Source/GDKTestGyms/BenchmarkGymGameModeBase.cpp
@@ -66,7 +66,7 @@ namespace
 	const FString MetricEnginePlatformLeftLabel = TEXT("engine_platform");
 	const FString MetricEnginePlatformRightLabel = TEXT("UnrealWorker");
 
-	const bool bEnableDensityBucketOutput = false;
+	const bool bEnableDensityBucketOutput = true;
 
 } // anonymous namespace
 
