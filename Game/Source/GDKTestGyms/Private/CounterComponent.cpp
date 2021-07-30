b139776f50c5e0c20edc406bf0115577cce4dfad
Michael Samiec <michaelsamiec@improbable.io>
2021-07-30 18:15:11 +0100

    PR feedback

--- Game/Source/GDKTestGyms/Private/CounterComponent.cpp
+++ Game/Source/GDKTestGyms/Private/CounterComponent.cpp
@@ -30,7 +30,7 @@ int32 UCounterComponent::GetActorClassCount(TSubclassOf<AActor> ActorClass) cons
 	const int32* CachedCount = CachedClassCounts.Find(ActorClass);
 	if (CachedCount == nullptr)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Could not get actor class count for class %s"), *ActorClass->GetName());
+		UE_LOG(LogCounterComponent, Warning, TEXT("Could not get actor class count for class %s"), *ActorClass->GetName());
 		return -1;
 	}
 
